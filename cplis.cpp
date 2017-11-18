d36e3fa97d080437ba1ac0c187be5744c95a869f
beastbywisdomslave <beast.incorporatedindustries@gmail.com>
2017-11-19 01:22:57 +0300

    initial commit

---
+++ cplis.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <iostream>
+using namespace std; 
+int main () 
+{
+        char layout; 
+        cout << "\nPython Or C++   " ; 
+        cin  >> layout;
+        return 0 ;
+        if(layout == "python ") {
+        	system ("start C:\Users\AH\Documents\start.bat");
+    	return 0;
+		} else {
+			system ("start C:\Users\AH\Documents\start.bat");
+    	return 0;
+		}
+}
